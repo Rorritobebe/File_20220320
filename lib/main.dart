24ee68dcefde4855a8cd719d4751bb831ade163c
Weapon279 <a202200356@utem.edu.mx>
2024-01-30 08:54:40 -0600

    Actualizacion

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class BrowsePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Browse')),
+      appBar: AppBar(title: Text('Reciente')),
       backgroundColor: Colors.grey[200],
       body: Column(
         children: [
@@ -23,7 +23,7 @@ class BrowsePage extends StatelessWidget {
             padding: EdgeInsets.all(4.0),
             child: TextField(
               decoration: InputDecoration(
-                hintText: 'Search...',
+                hintText: 'Busqueda...',
                 prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(),
               ),
@@ -32,26 +32,11 @@ class BrowsePage extends StatelessWidget {
           Expanded(
             child: ListView(
               children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.cloud),
-                  title: Text('iCloud Drive'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.phone_iphone),
-                  title: Text('On My iPhone'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.drive_eta),
-                  title: Text('Drive'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.cloud),
-                  title: Text('pCloud'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.delete),
-                  title: Text('Recently Deleted'),
-                ),
+                _buildListTile(context, Icons.cloud, 'iCloud Drive', 'Drive'),
+                _buildListTile(context, Icons.phone_iphone, 'On My iPhone', 'My phone'),
+                _buildListTile(context, Icons.drive_eta, 'Drive', 'Drive'),
+                _buildListTile(context, Icons.cloud, 'pCloud', 'pCloud'),
+                _buildListTile(context, Icons.delete, 'Recently Deleted', 'Deleted'),
               ],
             ),
           ),
@@ -59,25 +44,75 @@ class BrowsePage extends StatelessWidget {
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.recent_actors), label: 'Recents'),
-          BottomNavigationBarItem(icon: Icon(Icons.share), label:'Shared'),
-          BottomNavigationBarItem(icon: Icon(Icons.folder), label:'Browse'),
+          BottomNavigationBarItem(icon: Icon(Icons.recent_actors), label: 'Reciente'),
+          BottomNavigationBarItem(icon: Icon(Icons.share), label:'Compartir'),
+          BottomNavigationBarItem(icon: Icon(Icons.folder), label:'Explorador'),
         ],
         onTap:(index){
-          if(index == 1){
+          if(index == 2){
+            Navigator.push(context, MaterialPageRoute(builder:(context)=>BrowseDetailsPage()));
+          } else if(index == 1){
             Navigator.push(context, MaterialPageRoute(builder:(context)=>SharedPage()));
           }
         },
       ),
     );
   }
+
+  Widget _buildListTile(BuildContext context, IconData icon, String title, String additionalInfo) {
+    return InkWell(
+      onTap: () {
+        _navigateToAdditionalInfo(context, additionalInfo);
+      },
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+      ),
+    );
+  }
+
+  void _navigateToAdditionalInfo(BuildContext context, String additionalInfo) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AdditionalInfoPage(additionalInfo)),
+    );
+  }
 }
 
-class SharedPage extends StatelessWidget{
+class AdditionalInfoPage extends StatelessWidget {
+  final String additionalInfo;
+
+  AdditionalInfoPage(this.additionalInfo);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Weapon276")),
+      body: Center(
+        child: Text(additionalInfo),
+      ),
+    );
+  }
+}
+
+class BrowseDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Explorador")),
+      body: Center(
+        
+        child: Text("Weapon276"),
+      ),
+    );
+  }
+}
+
+class SharedPage extends StatelessWidget {
    @override
    Widget build(BuildContext context){
        return Scaffold(
-         appBar: AppBar(title : Text("Shared")),
+         appBar: AppBar(title : Text("Compartir")),
          backgroundColor: Colors.grey[200], 
        );
    }
